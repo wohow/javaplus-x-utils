d18ec126706089f3b32acef00df3bc75a347ceb5
deng <dell_@dell_-PC>
2015-09-18 16:53:27 +0800

    1

--- javaplus-x-utils/src/main/java/x/javaplus/collections/Maps.java
+++ javaplus-x-utils/src/main/java/x/javaplus/collections/Maps.java
@@ -1,11 +1,10 @@
 package x.javaplus.collections;
 
-import java.util.HashMap;
 
 public class Maps {
 	
-	public static <K,V> HashMap<K,V> newHashMap() {
-		return new HashMap<K,V>();
-	}
+//	public static <K,V> HashMap<K,V> newHashMap() {
+//		return new HashMap<K,V>();
+//	}
 	
 }
